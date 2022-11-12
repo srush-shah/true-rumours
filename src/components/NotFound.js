9735a21685483fb8a915fa637c75aa8a34e6cc40
Khushi Kothari <khushikothari.ict19@gmail.com>
2022-11-12 19:36:52 +0530

    Maximum UI done

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import { Navbar } from "./Navbar";
+import React from "react";
+
+export function NotFound() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-center font-normal text-4xl mt-32">Page not found</h1>
+    </>
+  );
+}
