44e4ac814b73876e7a152bb11063f2090273f034
Khushi Kothari <khushikothari.ict19@gmail.com>
2022-11-12 23:38:38 +0530

    Adding fetch code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-// import Fetch from "./pages/Fetch";
+import Fetch from "./pages/Fetch";
 import { NotFound } from "./components/NotFound";
 import SubCatPage from "./pages/SubCatPage";
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/fetch" element={<Fetch />}></Route> */}
+      <Route path="/fetch" element={<Fetch />}></Route>
       <Route path="/*" element={<NotFound />}></Route>
     </Routes>
   );
--- src/pages/Fetch.js
+++ src/pages/Fetch.js
@@ -1,55 +1,52 @@
-// // import React from "react";
+// import React from "react";
 
-// // function Fetch() {
-// //   return (
-// //     <div className="m-10 text-center">
-// //       <h1 className="text-4xl font-bold">Fetch</h1>
-// //     </div>
-// //   );
-// // }
-
-// // export default Fetch;
-
-// import React, { useState } from "react";
-// import axios from " axios";
 // function Fetch() {
-//   const getNews = () => {
-//     axios
-//       .get(
-//         "https://newsapi.org/v2/top-headlines?country=in&apiKey=6004b8fcb1604003"
-//       )
-//       .then((response) => {
-//         // console.log(response);
-//         setData(response.data.articles);
-//       });
-//     // .catch(()=> {})
-//   };
-
 //   return (
-//     // <div className="container my-3">
-//     //   <button className="btn btn-primary" onClick={getNews}>
-//     //     Fetch News{" "}
-//     //   </button>
-//     // <div>
-//     //  {
-//     //   <div>
-//     //   {
-//     //  data.map((value) => {
-//     //     <div className="col-3">
-//     //     <div className="card" style={{ width: "18rem" }}>
-//     //     <img src={value.urlToImage} className=" card-img-top" alt="..." />
-//     //     <div className="card-body">
-//     //     <h5 className="card-title">Card title</h5>
-//     //     <p className="card-text">Some quick example text to build on </p>
-//     //     <a href="#" class="btn btn-primary"> Go somewhere </a>
-//     //     </div>
-//     //     </div>
-//     //     </div>
-//     //  })
-//     // }
-//     // </div>
-//     <div>hello</div>
+//     <div className="m-10 text-center">
+//       <h1 className="text-4xl font-bold">Fetch</h1>
+//     </div>
 //   );
 // }
 
 // export default Fetch;
+
+import React, { useState } from "react";
+import axios from "axios";
+function Fetch() {
+  const [data, setData] = useState([]);
+  const getNews = () => {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=a5142567491b4ee7ba00902b29e50c2a&q=india"
+      )
+      .then((response) => {
+        console.log(response.data.articles);
+        setData(response.data.articles);
+      });
+    // .catch(()=> {})
+  };
+
+  let arr = [];
+  arr.push(
+    data.map((value) => {
+      let title = value.title;
+      return title;
+    })
+  );
+
+  return (
+    <div className="container my-3">
+      <button className="px-6 py-2 bg-blue-500" onClick={getNews}>
+        Fetch News{" "}
+      </button>
+      <h1>{arr}</h1>
+      <ul>
+        {arr.map((title) => {
+          return <li id={title}>{title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Fetch;
